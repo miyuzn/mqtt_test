728d341c17c422b86a996d61b2fab3964cd7961b
Zn Pu <miyuzn@hotmail.com>
2025-11-19 19:26:43 +0900

    1119-split away Android

--- android/provisioning/src/main/java/com/budiyev/android/codescanner/DecodeCallback.java
+++
@@ -1,8 +0,0 @@
-package com.budiyev.android.codescanner;
-
-import androidx.annotation.NonNull;
-import com.google.zxing.Result;
-
-public interface DecodeCallback {
-    void onDecoded(@NonNull Result result);
-}
